4284567ac830a53ec5311259210e19d7e43484e4
James Farrell <jamesfarrell@google.com>
2024-10-09 21:06:03 +0000

    Better output when analyzing updates.
    Be explicit about when there are no new versions, and when no problems
    were found.
    
    Bug: http://b/339424309
    Test: treehugger
    Change-Id: Ic3f69770b020fa7ee343c4c1850c4e11f9968f17

--- tools/external_crates/crate_tool/src/managed_repo.rs
+++ tools/external_crates/crate_tool/src/managed_repo.rs
@@ -620,10 +620,16 @@ impl ManagedRepo {
         ))?;
         let base_deps = base_version.android_version_reqs_by_name();
 
-        for version in cio_crate.versions_gt(krate.android_version()) {
+        let mut newer_versions = cio_crate.versions_gt(krate.android_version()).peekable();
+        if newer_versions.peek().is_none() {
+            println!("There are no newer versions of this crate.");
+        }
+        for version in newer_versions {
             println!("Version {}", version.version());
+            let mut found_problems = false;
             let parsed_version = semver::Version::parse(version.version())?;
             if !krate.android_version().is_upgradable_to(&parsed_version) {
+                found_problems = true;
                 if !krate.android_version().is_upgradable_to_relaxed(&parsed_version) {
                     println!("  Not semver-compatible, even by relaxed standards");
                 } else {
@@ -642,6 +648,7 @@ impl ManagedRepo {
                     &legacy_crates
                 };
                 if !cc.contains_name(dep.crate_name()) {
+                    found_problems = true;
                     println!(
                         "  Dep {} {} has not been imported to Android",
                         dep.crate_name(),
@@ -655,6 +662,7 @@ impl ManagedRepo {
                 }
                 for (_, dep_crate) in cc.get_versions(dep.crate_name()) {
                     if !req.matches_relaxed(dep_crate.version()) {
+                        found_problems = true;
                         println!(
                             "  Dep {} {} is not satisfied by v{} at {}",
                             dep.crate_name(),
@@ -667,6 +675,9 @@ impl ManagedRepo {
                         }
                     }
                 }
+            }
+            if !found_problems {
+                println!("  No problems found with this version.")
             }
         }
 
